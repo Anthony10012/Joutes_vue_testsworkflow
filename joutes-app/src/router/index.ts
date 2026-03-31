204f21474c1568819e8bb4cd108df80029664e83
Anthony Simond <pv22nbo@eduvaud.ch>
2026-03-31 14:21:42 +0200

    refactor: change name

--- joutes-app/src/router/index.ts
+++ joutes-app/src/router/index.ts
@@ -7,19 +7,8 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: SignInUser,
-    },
-
-    {
-      path: '/login',
-      name: 'login',
-      component: LogInUser,
-    },
-    {
       path: '/teams',
-      name: 'playerteamview',
+      name: 'PlayerTeamView',
       component: PlayerTeamView,
     },
     {
@@ -30,7 +19,17 @@ const router = createRouter({
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue'),
     },
-  ],
+    {
+      path: '/',
+      name: 'home',
+      component: SignInUser,
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: LogInUser,
+    },
+  ]
 })
 
 export default router

f3c359e31204d5184b7c7aa9f293311189658b8f
Anthony10012 <Anthony.Simond@eduvaud.ch>
2026-03-31 16:50:06 +0200

    Update vite.config.ts

--- joutes-app/vite.config.ts
+++ joutes-app/vite.config.ts
@@ -6,12 +6,14 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Joutes_vue/', // <--- TRÈS IMPORTANT
-  plugins: [vue()],
-})
+  base: '/Joutes_vue/', // Indique à Vite que le site est dans un sous-dossier
+  plugins: [
+    vue(),
+    vueDevTools(),
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
-})
+}) // La parenthèse doit se fermer TOUT À LA FIN
